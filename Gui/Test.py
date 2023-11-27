a08aa5ba4df5d259b407284ed35487544a9f4440
heping.zhang <pchpzh@hotmail.com>
2023-11-27 16:20:45 +0800

    backup

--- Gui/Test.py
+++ Gui/Test.py
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
 from PySide6.QtWidgets import (QApplication, QMainWindow, QMenu, QMenuBar,
-    QSizePolicy, QStatusBar, QWidget)
+    QSizePolicy, QStatusBar, QWidget, QMessageBox)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -30,58 +30,58 @@ class Ui_MainWindow(object):
         # set action for menu bar terminal items
         self.actionMenubar_About_Version = QAction(MainWindow)
         self.actionMenubar_About_Version.setObjectName(u"actionMenubar_About_Version")
+        # self.actionMenubar_About_Version.triggered.connect(self.show_version_diaglog)
 
-        self.actionRecent_project = QAction(MainWindow)
-        self.actionRecent_project.setObjectName(u"actionRecent_project")
+        self.actionMenubar_File_New_file = QAction(MainWindow)
+        self.actionMenubar_File_New_file.setObjectName(u"actionMenubar_File_New_file")
 
-        self.actionNew = QAction(MainWindow)
-        self.actionNew.setObjectName(u"actionNew")
+        self.actionMenubar_File_Open = QAction(MainWindow)
+        self.actionMenubar_File_Open.setObjectName(u"actionMenubar_File_Open")
 
-        self.actionNew_file = QAction(MainWindow)
-        self.actionNew_file.setObjectName(u"actionNew_file")
-
+        # set centralwidget for main window
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         MainWindow.setCentralWidget(self.centralwidget)
 
-
+        # new and setup menu bar
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 800, 21))
         self.menuFile = QMenu(self.menubar)
         self.menuFile.setObjectName(u"menuFile")
-        self.menuOpen_Project = QMenu(self.menuFile)
-        self.menuOpen_Project.setObjectName(u"menuOpen_Project")
         self.menuAbout = QMenu(self.menubar)
         self.menuAbout.setObjectName(u"menuAbout")
         MainWindow.setMenuBar(self.menubar)
+
+        # new and setup status bar
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
+        # assign action to menu bar items
         self.menubar.addAction(self.menuFile.menuAction())
         self.menubar.addAction(self.menuAbout.menuAction())
-        self.menuFile.addAction(self.menuOpen_Project.menuAction())
-        self.menuFile.addAction(self.actionNew_file)
-        self.menuOpen_Project.addAction(self.actionRecent_project)
-        self.menuOpen_Project.addAction(self.actionNew)
+        self.menuFile.addAction(self.actionMenubar_File_Open)
+        self.menuFile.addAction(self.actionMenubar_File_New_file)
         self.menuAbout.addAction(self.actionMenubar_About_Version)
 
         self.retranslateUi(MainWindow)
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
+    # def show_version_diaglog(self, MainWindow):
+    #     version_text = "My Application Version 1.0"
+    #     QMessageBox.aboutQt(MainWindow, "Version")
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"ArxmlTools", None))
         self.actionMenubar_About_Version.setText(QCoreApplication.translate("MainWindow", u"Version", None))
-        self.actionRecent_project.setText(QCoreApplication.translate("MainWindow", u"Recent project", None))
-        self.actionNew.setText(QCoreApplication.translate("MainWindow", u"New project", None))
-        self.actionNew_file.setText(QCoreApplication.translate("MainWindow", u"New file", None))
+        self.actionMenubar_File_New_file.setText(QCoreApplication.translate("MainWindow", u"New file...", None))
+        self.actionMenubar_File_Open.setText(QCoreApplication.translate("MainWindow", u"Open...", None))
         self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
-        self.menuOpen_Project.setTitle(QCoreApplication.translate("MainWindow", u"Open Project", None))
         self.menuAbout.setTitle(QCoreApplication.translate("MainWindow", u"About", None))
     # retranslateUi
+
 
 class MyWindow(QMainWindow):  # 这里的继承的父类一定要注意使用你定义UI窗体类型
     def __init__(self):
@@ -90,7 +90,10 @@ class MyWindow(QMainWindow):  # 这里的继承的父类一定要注意使用你
         self.ui = Ui_MainWindow()
         # 初始化界面
         self.ui.setupUi(self)
-
+        self.ui.actionMenubar_About_Version.triggered.connect(self.show_version_diaglog)
+    def show_version_diaglog(self):
+        version_text = "My Application Version 1.0"
+        QMessageBox.information(self.ui, "Version", version_text)
 
 # 启动窗口
 app = QApplication([])
