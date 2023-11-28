06a9011b21712e5d8f27442c27ef2cbd6cd09af5
heping.zhang <pchpzh@hotmail.com>
2023-11-28 18:38:13 +0800

    add base type view

--- Gui/ModelExplorer.py
+++ Gui/ModelExplorer.py
@@ -10,7 +10,7 @@ class ModelExplorer:
         self.view = AutosarView()
 
         # 解析arxml数据
-        AutosarData(path)
+        AutosarData(path, self.view)
 
         # grandson_item = QTreeWidgetItem(DataTypes, ["grandson1", "grandson1 data"])
 
@@ -22,8 +22,11 @@ class AutosarView(QTreeWidget):
         self.setColumnWidth(0, 200)
         # 添加根节点
         self.root_item = QTreeWidgetItem(self, ["AUTOSAR"])
+        # 设置根节点Icon
         icon = QIcon("Icon/autosar.jpg")
         self.root_item.setIcon(0, icon)
+        # 设置默认展开
+        self.root_item.setExpanded(True)
 
         # 添加一级子目录
         self.add_datatype_folder()
@@ -32,12 +35,25 @@ class AutosarView(QTreeWidget):
         self.add_Components_folder()
         self.add_Compositions_folder()
         self.add_Infrastruture_folder()
+        # self.add_datatype_basetype_folder()
+
+        self.datatypes_basetypes_folder = None
 
     def add_datatype_folder(self):
         # 添加data type子目录
-        DataTypes = QTreeWidgetItem(self.root_item, ["Data Types"])
+        self.datatypes_folder = QTreeWidgetItem(self.root_item, ["Data Types"])
         icon = QIcon("Icon/DataTypes.png")
-        DataTypes.setIcon(0, icon)
+        self.datatypes_folder.setIcon(0, icon)
+
+    def add_datatype_basetype_folder(self):
+        self.datatypes_basetypes_folder = QTreeWidgetItem(self.datatypes_folder, ["Base Types"])
+        icon = QIcon("Icon/DataTypes.png")
+        self.datatypes_basetypes_folder.setIcon(0, icon)
+    def add_basetype_item(self, name):
+        if self.datatypes_basetypes_folder is None:
+            self.add_datatype_basetype_folder()
+        basetypeitem = QTreeWidgetItem(self.datatypes_basetypes_folder, name)
+        pass
 
     def add_compumethod_folder(self):
         CompuMethod = QTreeWidgetItem(self.root_item, ["Compu Method"])
@@ -65,7 +81,7 @@ class AutosarView(QTreeWidget):
         Infrastructures.setIcon(0, icon)
 
 class AutosarData:
-    def __init__(self, workspace):
+    def __init__(self, workspace, view):
 
         self.arxml_files = []
         self.arxml_files = self.find_files_recursive(workspace, "arxml")
@@ -74,16 +90,17 @@ class AutosarData:
             ws.loadXML(arxml)
         for pack in ws.listPackages():
             for pack1 in ws.findall(pack):
-                self.package_parser(pack1)
+                self.package_parser(pack1, view)
 
-    def package_parser(self, package):
+    def package_parser(self, package, view):
         if package.subPackages:
             for son in package.subPackages:
-                self.package_parser(son)
+                self.package_parser(son, view)
         if package.elements:
             for ele in package.elements:
                 if isinstance(ele, autosar.datatype.SwBaseType):
                     print(ele.ref, ele.name, ele.parent, ele.nativeDeclaration, ele.size, ele.typeEncoding, ele.category)
+                    view.add_basetype_item([ele.name, ele.ref])
                     pass
                 if isinstance(ele, autosar.datatype.ImplementationDataType):
                     # print(ele.ref)
--- Gui/Test.py
+++ Gui/Test.py
@@ -1,16 +1,57 @@
-import glob
-import os
-def find_files_recursive(folder_path, file_extension):
-    pattern = os.path.join(folder_path, f"**/*.{file_extension}")
-    file_list = glob.glob(pattern, recursive=True)
-    return file_list
+from PySide6.QtWidgets import QApplication, QMainWindow, QTreeWidget, QTreeWidgetItem
+import sys
 
-# 例子：递归查找当前目录下所有的 .txt 文件
-folder_path = "D:\AutosarTutorial\ArxmlTools\Export"
-file_extension = "arxml"
-txt_files_recursive = find_files_recursive(folder_path, file_extension)
 
-print("Found .txt files recursively:")
-for file in txt_files_recursive:
-    print(file)
+class MyMainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        # 创建 QTreeWidget
+        tree_widget = QTreeWidget(self)
+
+        # 创建树形控件的列
+        tree_widget.setColumnCount(2)
+        tree_widget.setHeaderLabels(["Item", "Value"])
+
+        # 添加根节点
+        root_item = QTreeWidgetItem(tree_widget)
+        root_item.setText(0, "Root")
+        root_item.setText(1, "Root Value")
+
+        # 添加子节点
+        for i in range(3):
+            child_item = QTreeWidgetItem(root_item)
+            child_item.setText(0, f"Child {i}")
+            child_item.setText(1, f"Child Value {i}")
+
+        # 设置主窗口的中央部件为 QTreeWidget
+        self.setCentralWidget(tree_widget)
+
+        # 获取根节点下的所有子节点
+        child_items = self.get_child_items(root_item)
+        print("Child items of root:")
+        for item in child_items:
+            print(item.text(0))
+
+    def get_child_items(self, parent_item):
+        child_items = []
+        for i in range(parent_item.childCount()):
+            child_items.append(parent_item.child(i))
+        return child_items
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    # 创建主窗口对象
+    main_window = MyMainWindow()
+
+    # 显示主窗口
+    main_window.show()
+
+    # 运行应用程序
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
